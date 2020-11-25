bf7f80eab20427cce53cf5d6c1be28857344067a
Your Name <ckholmes5@gmail.com>
2020-11-24 23:44:01 -0500

    prodz

--- importdata.py
+++ importdata.py
@@ -18,63 +18,59 @@ out = ''
 
 while True:
     try:
-        sleep(20)
-        ROUTE = 'C'
-        feed = SubwayFeed.get(ROUTE, api_key=blah_blah)
-        feed = feed.dict()
-        current_time = datetime.datetime.now()
-        for stop in STOP_IDS:
-            proceed = False
-            route = UNIQUE_STOPS[STOP_IDS[stop]]
-            for entity in feed['entity']:
-                if entity['trip_update'] and entity['trip_update']['stop_time_update'] is not None:
-                    stops = [update['stop_id'] for update in entity['trip_update']['stop_time_update']]
-                    if (route and route in stops) | (not route):
-                        proceed = True
-                        for update in entity['trip_update']['stop_time_update']:
-                            if update['stop_id'] == stop:
-                                time = update['arrival']['time']
-                                current_time = datetime.datetime.now(timezone.utc)
-                                time = math.trunc(((time - current_time).total_seconds()) / 60)
-                                times.append(time)
+        sleep(1)
+        for route in ['B','C']:
+            ROUTE = route
+            feed = SubwayFeed.get(ROUTE, api_key=blah_blah)
+            feed = feed.dict()
+            current_time = datetime.datetime.now()
+            for stop in STOP_IDS:
+                proceed = False
+                route = UNIQUE_STOPS[STOP_IDS[stop]]
+                for entity in feed['entity']:
+                    if entity['trip_update'] and entity['trip_update']['stop_time_update'] is not None:
+                        stops = [update['stop_id'] for update in entity['trip_update']['stop_time_update']]
+                        if (route and route in stops) | (not route):
+                            proceed = True
+                            for update in entity['trip_update']['stop_time_update']:
+                                if update['stop_id'] == stop:
+                                    time = update['arrival']['time']
+                                    current_time = datetime.datetime.now(timezone.utc)
+                                    time = math.trunc(((time - current_time).total_seconds()) / 60)
+                                    times.append(time)
+                        else:
+                            continue
+
+                if proceed:
+                    times.sort()
+                    for time in times:
+                        if time < 0:
+                            times.remove(time)
+                    for time in times[:NUM_TRAINS]:
+                        out+=str(time)
+                        out+=str(', ')
+                    out = out[:-2]
+                    print(STOP_IDS)
+                    if route:
+                        staticimg = Image.open('/home/pi/Desktop/git/subway_data/staticimages/' + STOP_IDS[stop] + '.ppm')
                     else:
-                        continue
+                        print('herw!')
+                        staticimg = Image.open('/home/pi/Desktop/git/subway_data/staticimages/' + stop[0] + stop[3] + '.ppm')
 
-            if proceed:
-                times.sort()
-                for time in times:
-                    if time < 0:
-                        times.remove(time)
-                for time in times[:NUM_TRAINS]:
-                    out+=str(time)
-                    out+=str(', ')
-                out = out[:-2]
-                if route:
-                    staticimg = Image.open('staticimages/' + STOP_IDS[stop] + '.ppm')
-                else:
-                    staticimg = Image.open('staticimages/' + stop[0] + stop[3] + '.ppm')
-
-                draw = ImageDraw.Draw(staticimg)
-                #font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf', 12)
-                font = ImageFont.truetype('/Library/Fonts/Helvetica-Regular.ttf', 14)
-                draw.text((22, 3), out,(200,200,200),font=font)
-                staticimg.save('dynamicimages/dynamictime.ppm')
-                times = []
-                out = ''
-            # os.system('./rpi-rgb-led-matrix/led-matrix -r 16 -c 2 -t 5 -b 50 -D 1 -m 5000 dynamicimages/dynamictime.ppm')
+                    draw = ImageDraw.Draw(staticimg)
+                    font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf', 60)
+                    draw.text((25, 4), out,(255,255,255))
+                    staticimg.save('/home/pi/Desktop/git/subway_data/dynamicimages/dynamictime.ppm')
+                    times = []
+                    out = ''
+                os.system('sudo /home/pi/Desktop/git/subway_data/rpi-rgb-led-matrix/examples-api-use/./demo --led-rows=16 --led-cols=32 --led-chain=2 -t 5 --led-brightness=20 --led-slowdown-gpio=4 -D 1 -m 0 /home/pi/Desktop/git/subway_data/dynamicimages/dynamictime.ppm')
     except Exception:
         print (traceback.format_exc())
 
 
 
 
-    # Need to classify it as a B or a C stop
-    # Know how to classify based on the other stops
-    #
+        # Need to classify it as a B or a C stop
+        # Know how to classify based on the other stops
+        
 
-
-
-
-    # Need to classify it as a B or a C stop
-    # Know how to classify based on the other stops
-    #
