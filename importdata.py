08b434848e9de178493fc7db1c65dd8efae96c7d
Christian Holmes <yourgitemail@example.com>
2020-11-27 16:53:24 -0500

    threading

--- importdata.py
+++ importdata.py
@@ -19,12 +19,12 @@ out = ''
 
 def func1():
     os.system('sudo /home/pi/Desktop/git/subway_data/rpi-rgb-led-matrix/examples-api-use/./demo --led-rows=16 --led-cols=32 --led-chain=2 -t 10 --led-brightness=20 --led-slowdown-gpio=4 -D 1 -m 0 /home/pi/Desktop/git/subway_data/dynamicimages/dynamictime.ppm')
+    sleep(5)
 
 def func2(ROUTE):
     print('here we are!')
     global feed
     feed = SubwayFeed.get(ROUTE, api_key=blah_blah)
-    import pdb; pdb.set_trace()
 
 counter = 0
 
