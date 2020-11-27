dc41959acdfdcd48b3388219e912f43dcce60b68
Christian Holmes <yourgitemail@example.com>
2020-11-27 17:08:06 -0500

    threading

--- importdata.py
+++ importdata.py
@@ -75,7 +75,7 @@ while True:
             times = []
             out = ''
 
-            os.system('sudo /home/pi/Desktop/git/subway_data/rpi-rgb-led-matrix/examples-api-use/./demo --led-rows=16 --led-cols=32 --led-chain=2 -t 10 --led-brightness=20 --led-slowdown-gpio=4 -D 1 -m 0 /home/pi/Desktop/git/subway_data/dynamicimages/dynamictime.ppm')
+            # os.system('sudo /home/pi/Desktop/git/subway_data/rpi-rgb-led-matrix/examples-api-use/./demo --led-rows=16 --led-cols=32 --led-chain=2 -t 10 --led-brightness=20 --led-slowdown-gpio=4 -D 1 -m 0 /home/pi/Desktop/git/subway_data/dynamicimages/dynamictime.ppm')
             counter += 1
 
 
